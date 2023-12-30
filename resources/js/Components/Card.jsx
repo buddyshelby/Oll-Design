95c843501e6583e5a6dd8a7f5eb684360158c826
FR13ND5 <nuno180202@gmail.com>
2023-12-31 02:30:17 +0700

    Revision v1.1

--- resources/js/Components/Card.jsx
+++ resources/js/Components/Card.jsx
@@ -1,7 +1,7 @@
-export default function Card({ children, color, rounded, padding }) {
+export default function Card({ children, color, rounded, padding, className = '' }) {
     return (
         <div
-            className={`${color === undefined ? "bg-[#f0f0f0]" : color} ${rounded && rounded} ${padding && padding}`}
+            className={`${color === undefined ? "bg-[#f0f0f0]" : color} ${rounded && rounded} ${padding && padding} ${className}`}
         >
             {children}
         </div>
