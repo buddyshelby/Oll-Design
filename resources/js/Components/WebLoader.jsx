ccff307929a77dd2585c62a35c1455044c6e9309
buddyshelby <mtegar057@gmail.com>
2024-04-08 14:29:46 +0700

    Add Effect
    Add Glitch Effect on Logo Loading

--- resources/js/Components/WebLoader.jsx
+++ resources/js/Components/WebLoader.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import style from './web-loader.module.css'
 
 export default function WebLoader() {
     return (
         <div className="relative w-screen h-screen bg-[#f0f0f0]">
-            <div className="flex justify-center align-items-center h-full">
+            <div className={`flex justify-center align-items-center h-full ${style['the--cubic']}`}>
+                <img
+                    src="assets/logo/logo-square.png"
+                    alt="logo"
+                    className="w-3/12"
+                />
                 <img
                     src="assets/logo/logo-square.png"
                     alt="logo"
