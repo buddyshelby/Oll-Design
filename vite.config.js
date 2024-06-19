506a5de6baee98b027724fd0a135ba7f37b37658
buddyshelby <mtegar057@gmail.com>
2024-06-20 02:47:38 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 //     server: {
-// 	host: 'olldesign.jp'
+// 	host: 'olldesign.jp'a
 // },
     plugins: [
         laravel({
