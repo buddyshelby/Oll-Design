1e8eb98c1520dae32f927c5c76abf15389d49160
buddyshelby <mtegar057@gmail.com>
2024-06-23 21:30:10 +0700

    tes

--- resources/js/Pages/Panel/Galleries/ListGalleries.jsx
+++ resources/js/Pages/Panel/Galleries/ListGalleries.jsx
@@ -93,6 +93,7 @@ export default function ListGalleries() {
         setSelectedGallery(selected);
         setUpdatedData({
             Name: selected.Name,
+            Name: selected.Name,
             Date: selected.Date,
             DescriptionJp: selected.DescriptionJp,
             DescriptionEn: selected.DescriptionEn,
