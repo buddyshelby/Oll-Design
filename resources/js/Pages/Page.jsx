dc6013a147a0aa54a3aeb9c273ea9fcdf7a39dc1
buddyshelby <mtegar057@gmail.com>
2024-07-16 23:22:17 +0700

    Update Page.jsx

--- resources/js/Pages/Page.jsx
+++ resources/js/Pages/Page.jsx
@@ -191,7 +191,7 @@ const Page = ({ onLoad = false, children, galleryDetailView }) => {
             <MediaQuery query="(max-width: 768px)">
                 {({ matches }) => (
                     <>
-                        <Head title="OLLDESIVE" />
+                        <Head title="OLLDESIGN" />
                         {matches ? (
                             currentPath === "/" ? (
                                 <div className={`flex-col ${galleryDetailView ? 'fixed' : ''}`}>
