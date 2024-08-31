48ba17441b7074aaf332b7cc45acc4717a47dabe
buddyshelby <mtegar057@gmail.com>
2024-08-31 13:32:22 +0700

    Update Page.jsx

--- resources/js/Pages/Page.jsx
+++ resources/js/Pages/Page.jsx
@@ -237,7 +237,7 @@ const Page = ({ children, galleryDetailView, imageShow, hideLoad, loadPercent })
                                                 <div className={`w-full h-full absolute flex flex-col top-0 left-0 justify-center items-center ${!hideLoad ? 'opacity-100' : 'opacity-0' } transition-all duration-1000`}>
                                                     <div className="w-16 h-16 border-4 border-transparent border-t-gray-600 rounded-full animate-spin"></div>
                                                     <span className="mt-4 text-white">Wait for a moment, it will take longer than usual...</span>
-                                                    <span className="mt-2 text-xl text-white">{loadPercent}%</span>
+                                                    <span className="mt-2 text-xl text-white"> {loadPercent}% </span>
                                                 </div>
                                             )}
                                         </div>
