cb8b2c7bcc9a6fc11bf4203575030bd382f76c40
buddyshelby <mtegar057@gmail.com>
2024-12-19 15:54:25 +0700

    Progress Homepage 10%

--- resources/js/Pages/Homepage/Homepage.jsx
+++ resources/js/Pages/Homepage/Homepage.jsx
@@ -1,53 +1,81 @@
-import React, { useState, useEffect, Fragment } from "react";
-import { useTranslation } from "react-i18next";
-
-import MediaQuery from "@/Components/MediaQuery";
+import React, { useState, useEffect, Fragment, useRef } from "react";
 
 import Page from "../Page";
 
 import classes from "./Homepage.module.css";
+import MediaQuery from "@/Components/MediaQuery";
 
-const Company = () => {
-    let wordLength = 0
-    const { i18n } = useTranslation();
-    const [isLanguage, setIsLanguage] = useState(Object.values(i18n.store.data)[0].translation);
+const Homepage = () => {
 
-    const [maxSizeDiv, setMaxSizeDiv] = useState(0)
-    const elementAll = document.querySelectorAll('div');
+    const firstQuestionRef = useRef(null)
+    const [firstQuestionDesc, setFirstQuestionDesc] = useState(0)
 
     useEffect(() => {
-        const checkWord = Array.from(elementAll).find(item => item.innerHTML === maxSizeDiv)
-        if (checkWord !== undefined)
-            setMaxSizeDiv(checkWord.clientWidth + 16)
-    }, [maxSizeDiv])
-
-    useEffect(() => {
-        if (i18n.language === "jp") {
-            setIsLanguage(i18n.store.data.jp.translation);
-        } else if (i18n.language === "ja") {
-            setIsLanguage(i18n.store.data.jp.translation);
-        } else if (i18n.language === "en") {
-            setIsLanguage(i18n.store.data.en.translation);
-        } else if (i18n.language === "en-US") {
-            setIsLanguage(i18n.store.data.en.translation);
-        } else if (i18n.language === "ch") {
-            setIsLanguage(i18n.store.data.ch.translation);
-        } else {
-            setIsLanguage(Object.values(i18n.store.data)[0].translation);
+        if (firstQuestionRef.current) {
+            setFirstQuestionDesc(firstQuestionRef.current.clientWidth)
         }
-    }, [i18n.language]);
+    }, [firstQuestionRef.current?.clientWidth])
+    
+    const handleResize = () => {
+        if (firstQuestionRef.current) {
+            setFirstQuestionDesc(firstQuestionRef.current.clientWidth)
+        }
+    };
 
-    const instagracmClick = () => {
-        window.open('https://www.instagram.com/olldesign_1010', '_blank')
-    }
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
 
     return (
         <Page>
-            <div className="w-full h-full">
-                tes
-            </div>
+            <MediaQuery query="(max-width: 768px)">
+            {({ matches }) => (
+                <>
+                {matches ?
+                
+                <div className="w-full h-full bg-green-500 flex flex-col items-center" style={{ padding: '4vw 2vw 0 2vw', opacity: firstQuestionDesc === 0 ? '0' : '1' }}>
+                    <div className="w-fit flex h-max">
+                        <div className="flex justify-center items-center" style={{ padding: '0 2vw 0 0', fontSize: '15vw' }}>
+                            Q
+                        </div>
+                        <div className="flex flex-col">
+                            <div ref={firstQuestionRef} style={{ fontSize: '4vw', letterSpacing: '0.2vw' }} className="w-fit">グラフィックだけはしてくれないの？</div>
+                            <div className="flex justify-center items-center" style={{ width: `${firstQuestionDesc}px`, fontSize: '2.4vw', letterSpacing: '0.2vw' }}>
+                            設計業務だけに付随する事業だと思われがちで、よくこの質問をお客様か
+                            ら頂きますが応えはもちろん「はい、よろこんで」。
+                            それが当グラフィック事業専用ページを開設した経緯です。
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                :
+
+                <div className="w-full h-full bg-green-500 flex flex-col items-center" style={{ padding: '4vw 5vw 0 5vw', opacity: firstQuestionDesc === 0 ? '0' : '1' }}>
+                    <div className="w-fit flex h-max">
+                        <div className="flex justify-center items-center" style={{ padding: '0 2vw 0 0', fontSize: '8vw' }}>
+                            Q
+                        </div>
+                        <div className="flex flex-col">
+                            <div ref={firstQuestionRef} style={{ fontSize: '2vw', letterSpacing: '0.2vw' }} className="w-fit">グラフィックだけはしてくれないの？</div>
+                            <div className="flex justify-center items-center" style={{ width: `${firstQuestionDesc}px`, fontSize: '1.3vw' }}>設計業務だけに付随する事業だと思われがちで、よくこの質問をお客様か
+    ら頂きますが応えはもちろん「はい、よろこんで」。
+    それが当グラフィック事業専用ページを開設した経緯です。</div>
+                        </div>
+                    </div>
+                </div>
+
+                }
+                </>
+            )}
+            </MediaQuery>
         </Page>
     );
 };
 
-export default Company;
+export default Homepage;
